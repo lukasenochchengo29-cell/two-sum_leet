bcf5ff557f6f7b85db5e0abded5c7aeeac432649
lukas enoch chengo <237792536+lukasenochchengo29-cell@users.noreply.github.com>
2026-09-06 22:40:28 +0300

    runningSum Done

---
+++ Running-Sum-of-1d-Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        result = []
+
+        for num in nums:
+            total += num
+            result.append(total)
+
+        return result
